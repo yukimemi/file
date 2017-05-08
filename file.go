420a266d525b08e3ae4f08581bc751d184197793
yukimemi <yukimemi@gmail.com>
2017-05-09 07:30:22 +0900

    :+1: Add file copy func.

--- file.go
+++ file.go
@@ -2,8 +2,10 @@ package file
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"runtime"
@@ -519,4 +521,80 @@ func compileRegexps(opt Option) (Option, error) {
 		}
 	}
 	return opt, err
+}
+
+// OsCopy is os specific copy command.
+func OsCopy(src, dst string) (*core.Cmd, error) {
+
+	var (
+		copyCmd string
+		copyArg []string
+	)
+	src = filepath.FromSlash(src)
+	dst = filepath.FromSlash(dst)
+	if runtime.GOOS == "windows" {
+		copyCmd = "cmd"
+		copyArg = []string{"/c", "copy"}
+	} else {
+		copyCmd = "cp"
+		copyArg = []string{"-pv"}
+	}
+
+	copyArg = append(copyArg, src, dst)
+
+	cmd := core.Cmd{Cmd: exec.Command(copyCmd, copyArg...)}
+	err := cmd.CmdRun()
+	if err != nil {
+		return &cmd, err
+	}
+	if cmd.ExitCode != 0 {
+		return &cmd, fmt.Errorf("copy command exit code: [%v]", cmd.ExitCode)
+	}
+
+	return &cmd, nil
+}
+
+// Copy is file copy using io.Copy.
+func Copy(src, dst string, overwrite bool) (int64, error) {
+
+	src = filepath.FromSlash(src)
+	dst = filepath.FromSlash(dst)
+	fs, err := os.Open(src)
+	if err != nil {
+		return -1, err
+	}
+	defer fs.Close()
+	fss, err := fs.Stat()
+	if err != nil {
+		return -1, err
+	}
+
+	if IsExist(dst) && !overwrite {
+		fds, err := os.Stat(dst)
+		if err != nil {
+			return -1, err
+		}
+
+		if fss.Size() == fds.Size() && fss.ModTime() == fds.ModTime() {
+			return 0, nil
+		}
+	}
+
+	ds, err := os.Create(dst)
+	if err != nil {
+		return -1, err
+	}
+	defer ds.Close()
+
+	n, err := io.Copy(ds, fs)
+	if err != nil {
+		return -1, err
+	}
+
+	err = os.Chtimes(dst, fss.ModTime(), fss.ModTime())
+	if err != nil {
+		return -1, err
+	}
+
+	return n, nil
 }
